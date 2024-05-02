5a65c796ba1567100df28d7a3cda441d7652fcb8
David Martínez <dave.7u7@gmail.com>
2024-05-02 02:21:35 -0600

    Primeros pasos creando el modal

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,30 @@ function crearGaleria() {
 
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen galería'
+
+        // Event Handler
+        imagen.onclick = function() {
+            mostrarImagen(i)
+        }
+
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(i) {
+    // Generar Modal
+
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    // Agregar al HTML
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+
+    modal?.remove()
 }
