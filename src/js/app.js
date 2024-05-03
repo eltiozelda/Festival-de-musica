756601de86da94b212683fa942bffa8c8cc41879
David Martínez <dave.7u7@gmail.com>
2024-05-03 02:20:38 -0600

    Minificando CSS y JS; Agregando Lazy loading a imagenes

--- src/js/app.js
+++ src/js/app.js
@@ -26,8 +26,12 @@ function crearGaleria() {
 
     for (let i = 1; i <= CANTIDAD_IMAGENES; i++){
         const imagen = document.createElement('IMG')
+        imagen.loading = 'lazy'
+        imagen.width = "300"
+        imagen.height = '200'
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen galería'
+        
 
         // Event Handler
         imagen.onclick = function() {
